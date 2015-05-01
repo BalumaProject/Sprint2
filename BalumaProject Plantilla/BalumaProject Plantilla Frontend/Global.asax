8d63d569c30268836afe6b50b84ce2ae59486d72
Gynoid-M <marina.boluda@gmail.com>
2015-05-01 20:58:29 +0200

    Limpieza

--- BalumaProject Plantilla/BalumaProject Plantilla Frontend/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BalumaProject_Plantilla_Frontend.Global" Language="C#" %>
--- BalumaProjectGen/BalumaProjectGenNHibernate/Enumerated/BalumaProject/EstadoPedidoEnum.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-namespace BalumaProjectGenNHibernate.Enumerated.BalumaProject
-{
-public enum EstadoPedidoEnum { entregado=1, en_curso=2, cancelado=3 };
-}
