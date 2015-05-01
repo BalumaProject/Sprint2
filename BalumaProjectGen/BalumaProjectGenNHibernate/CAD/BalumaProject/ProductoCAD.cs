8978ff13479069471f2822b377fa76f5f17facca
Manu Pascual <mpascual1988@gmail.com>
2015-05-01 18:53:59 +0200

    formatea codigo

--- BalumaProjectGen/BalumaProjectGenNHibernate/CAD/BalumaProject/ProductoCAD.cs
+++ BalumaProjectGen/BalumaProjectGenNHibernate/CAD/BalumaProject/ProductoCAD.cs
@@ -11,125 +11,128 @@ using BalumaProjectGenNHibernate.Exceptions;
 
 namespace BalumaProjectGenNHibernate.CAD.BalumaProject
 {
-public partial class ProductoCAD : BasicCAD, IProductoCAD
-{
-public ProductoCAD() : base ()
-{
-}
-
-public ProductoCAD(ISession sessionAux) : base (sessionAux)
-{
-}
-
-
-
-public ProductoEN ReadOIDDefault (int idProducto)
-{
-        ProductoEN productoEN = null;
-
-        try
+    public partial class ProductoCAD : BasicCAD, IProductoCAD
+    {
+        public ProductoCAD()
+            : base()
         {
-                SessionInitializeTransaction ();
-                productoEN = (ProductoEN)session.Get (typeof(ProductoEN), idProducto);
-                SessionCommit ();
         }
 
-        catch (Exception ex) {
-                SessionRollBack ();
+        public ProductoCAD(ISession sessionAux)
+            : base(sessionAux)
+        {
+        }
+
+        public ProductoEN ReadOIDDefault(int idProducto)
+        {
+            ProductoEN productoEN = null;
+
+            try
+            {
+                SessionInitializeTransaction();
+                productoEN = (ProductoEN)session.Get(typeof(ProductoEN), idProducto);
+                SessionCommit();
+            }
+
+            catch (Exception ex)
+            {
+                SessionRollBack();
                 if (ex is BalumaProjectGenNHibernate.Exceptions.ModelException)
-                        throw ex;
-                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException ("Error in ProductoCAD.", ex);
+                    throw ex;
+                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException("Error in ProductoCAD.", ex);
+            }
+
+
+            finally
+            {
+                SessionClose();
+            }
+
+            return productoEN;
         }
 
 
-        finally
+        public int CrearProducto(ProductoEN producto)
         {
-                SessionClose ();
-        }
+            try
+            {
+                SessionInitializeTransaction();
+                if (producto.Administrador != null)
+                {
+                    producto.Administrador = (BalumaProjectGenNHibernate.EN.BalumaProject.AdministradorEN)session.Load(typeof(BalumaProjectGenNHibernate.EN.BalumaProject.AdministradorEN), producto.Administrador.NIF);
 
-        return productoEN;
-}
-
-
-public int CrearProducto (ProductoEN producto)
-{
-        try
-        {
-                SessionInitializeTransaction ();
-                if (producto.Administrador != null) {
-                        producto.Administrador = (BalumaProjectGenNHibernate.EN.BalumaProject.AdministradorEN)session.Load (typeof(BalumaProjectGenNHibernate.EN.BalumaProject.AdministradorEN), producto.Administrador.NIF);
-
-                        producto.Administrador.Producto.Add (producto);
+                    producto.Administrador.Producto.Add(producto);
                 }
 
-                session.Save (producto);
-                SessionCommit ();
-        }
+                session.Save(producto);
+                SessionCommit();
+            }
 
-        catch (Exception ex) {
-                SessionRollBack ();
+            catch (Exception ex)
+            {
+                SessionRollBack();
                 if (ex is BalumaProjectGenNHibernate.Exceptions.ModelException)
-                        throw ex;
-                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException ("Error in ProductoCAD.", ex);
+                    throw ex;
+                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException("Error in ProductoCAD.", ex);
+            }
+
+
+            finally
+            {
+                SessionClose();
+            }
+
+            return producto.IdProducto;
         }
 
-
-        finally
+        public void ModificarProducto(ProductoEN producto)
         {
-                SessionClose ();
-        }
-
-        return producto.IdProducto;
-}
-
-public void ModificarProducto (ProductoEN producto)
-{
-        try
-        {
-                SessionInitializeTransaction ();
-                ProductoEN productoEN = (ProductoEN)session.Load (typeof(ProductoEN), producto.IdProducto);
+            try
+            {
+                SessionInitializeTransaction();
+                ProductoEN productoEN = (ProductoEN)session.Load(typeof(ProductoEN), producto.IdProducto);
 
                 productoEN.Precio = producto.Precio;
 
-                session.Update (productoEN);
-                SessionCommit ();
-        }
+                session.Update(productoEN);
+                SessionCommit();
+            }
 
-        catch (Exception ex) {
-                SessionRollBack ();
+            catch (Exception ex)
+            {
+                SessionRollBack();
                 if (ex is BalumaProjectGenNHibernate.Exceptions.ModelException)
-                        throw ex;
-                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException ("Error in ProductoCAD.", ex);
+                    throw ex;
+                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException("Error in ProductoCAD.", ex);
+            }
+
+            finally
+            {
+                SessionClose();
+            }
         }
-
-
-        finally
+        public void BorrarProducto(int idProducto)
         {
-                SessionClose ();
-        }
-}
-public void BorrarProducto (int idProducto)
-{
-        try
-        {
-                SessionInitializeTransaction ();
-                ProductoEN productoEN = (ProductoEN)session.Load (typeof(ProductoEN), idProducto);
-                session.Delete (productoEN);
-                SessionCommit ();
-        }
+            try
+            {
+                SessionInitializeTransaction();
+                ProductoEN productoEN = (ProductoEN)session.Load(typeof(ProductoEN), idProducto);
+                session.Delete(productoEN);
+                SessionCommit();
+            }
 
-        catch (Exception ex) {
-                SessionRollBack ();
+            catch (Exception ex)
+            {
+                SessionRollBack();
                 if (ex is BalumaProjectGenNHibernate.Exceptions.ModelException)
-                        throw ex;
-                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException ("Error in ProductoCAD.", ex);
-        }
+                    throw ex;
+                throw new BalumaProjectGenNHibernate.Exceptions.DataLayerException("Error in ProductoCAD.", ex);
+            }
 
-
-        finally
-        {
-                SessionClose ();
+            finally
+            {
+                SessionClose();
+            }
         }
-}
-}
+    }
 }
