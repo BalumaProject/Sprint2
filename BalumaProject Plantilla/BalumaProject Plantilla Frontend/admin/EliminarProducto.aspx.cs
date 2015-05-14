0cb0cd3e6d735488a2d0a550c6bba66546d1f074
Manu Pascual <mpascual1988@gmail.com>
2015-05-14 20:27:30 +0200

    el administrador puede eliminar productos

---
+++ BalumaProject Plantilla/BalumaProject Plantilla Frontend/admin/EliminarProducto.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BalumaProjectGenNHibernate.EN.BalumaProject;
+using BalumaProjectGenNHibernate.CEN.BalumaProject;
+
+namespace BalumaProject_Plantilla_Frontend.admin
+{
+    public partial class EliminarProducto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request["id"] != null)
+            {
+                Eliminar();
+            }
+        }
+
+        protected IList<ProductoEN> ObtenerProductos()
+        {
+            var p = new ProductoCEN();
+            return p.DameTodosLosProductos(0, 100);
+        }
+
+        private void Eliminar()
+        {
+            var id = int.Parse(Request["id"]);
+            var cen = new ProductoCEN();
+            cen.BorrarProducto(id);
+            Response.Redirect("EliminarProducto.aspx");
+        }
+    }
+}
