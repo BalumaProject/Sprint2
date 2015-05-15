e971ed0174c0beb6655d3ff113a421e941dabab7
Manu Pascual <mpascual1988@gmail.com>
2015-05-15 14:01:42 +0200

    el catalogo ahora se genera desde la vista

--- BalumaProject Plantilla/BalumaProject Plantilla Frontend/Catalogo.aspx
+++ BalumaProject Plantilla/BalumaProject Plantilla Frontend/Catalogo.aspx
@@ -1,63 +1,76 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Catalogo.aspx.cs" Inherits="BalumaProject_Plantilla_Frontend.Catalogo" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true"
+    CodeBehind="Catalogo.aspx.cs" Inherits="BalumaProject_Plantilla_Frontend.Catalogo" %>
+
 <%@ MasterType VirtualPath="~/Site1.Master" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
- 
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
-<div class="main_bg">
-<div class="wrap">
-<div class="main">
-
-<form ID = "form" runat="server">
-
-	<div class="panel-buscar">
-    
-    <div class="top-buscar">
-    <asp:Label Text="Filtro de búsqueda" runat="server"/>
+<form id="form" runat="server">
+    <div class="main_bg">
+        <div class="wrap">
+            <div class="main">
+                
+                <div class="panel-buscar">
+                    <div class="top-buscar">
+                        <asp:Label Text="Filtro de búsqueda" runat="server" />
+                    </div>
+                    <asp:Label Text="Por Categoria" runat="server" CssClass="etiqueta-buscar" />
+                    <asp:DropDownList ID="lista" runat="server" CssClass="lista-categoria">
+                        <asp:ListItem Text="Todos"></asp:ListItem>
+                        <asp:ListItem Text="Latina"></asp:ListItem>
+                        <asp:ListItem Text="De cuchillo"></asp:ListItem>
+                        <asp:ListItem Text="Cuadra"></asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:Label Text="Por Precio" runat="server" CssClass="etiqueta-buscar" />
+                    <asp:TextBox ID="precio" runat="server" CssClass="lista-categoria"></asp:TextBox>
+                    <div class="registration_form">
+                        <div class="boton-buscar">
+                            <asp:Button OnClick="BuscarProductos" Text="Buscar" runat="server" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <% var contador = 0; %>
+            <div class="grids_of_3">
+                <% var prods = ObtenerProductos(); %>
+                <% foreach (var p in prods) %>
+                <% { %>
+                <% if (contador == 3) %>
+                <%{ %>
+                <div class="clear">
+                </div>
+                <div class="grids_of_3">
+                    <% contador = 0; %>
+                    <%} %>
+                    <div class="grid1_of_3">
+                        <a href="#">
+                            <img src="Content/images/bavaria.jpg" alt="Imagen del producto" />
+                        </a>
+                        <%= p.Nombre %>
+                        <span class="price"><%= p.Precio %> €</span> 
+                        <asp:Button Text="Añadir" OnClick="anyadeCarrito" runat="server" />
+                    </div>
+                    <% contador++; %>
+                    <% } %>
+                </div>
+            </div>
+            <div class="clear" />
     </div>
-    <asp:Label Text="Por Categoria" runat="server" CssClass="etiqueta-buscar"/>
-    <asp:DropDownList id = "lista" runat="server" CssClass="lista-categoria">
-    <asp:ListItem Text="Todos"></asp:ListItem>
-    <asp:ListItem   Text="Latina"></asp:ListItem>
-    <asp:ListItem  Text="De cuchillo"></asp:ListItem>
-    <asp:ListItem  Text="Cuadra"></asp:ListItem>
-    
-    </asp:DropDownList>
-    <asp:Label Text="Por Precio" runat="server" CssClass="etiqueta-buscar"/>
-    <asp:TextBox id = "precio" runat="server" CssClass="lista-categoria"></asp:TextBox>
-    
-    <div class="registration_form">
-    <div class="boton-buscar">
-    <asp:Button Onclick = "buscar_productos" Text="Buscar" runat="server"/>
+    </form>
+    <!-- start footer -->
+    <div class="footer_bg">
+        <div class="wrap">
+            <div class="footer">
+                <div class="f_nav1">
+                    <ul>
+                        <li><a href="~/indice.aspx" runat="server">Inicio/</a></li>
+                        <li><a href="#">Términos y condiciones /</a></li>
+                        <li><a href="#">Contáctanos</a></li>
+                    </ul>
+                </div>
+                <div class="clear">
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-    </div>
-    
-
-</div>
-
-<%=content%> <!--contenido autogenerado en  Catalogo.aspx.cs para items dinámicos-->
-	
-</div>
-</div>
-
-</form>
-<!-- start footer -->
-<div class="footer_bg">
-<div class="wrap">
-<div class="footer">
-
-	<div class="f_nav1">
-		<ul>
-			<li><a href="~/indice.aspx" runat="server">Inicio/</a></li>
-			<li><a href="#">Términos y condiciones /</a></li>
-			<li><a href="#">Contáctanos</a></li>
-		</ul>
-		</div>
-	
-	<div class="clear"></div>
-</div>
-</div>
-</div>
 </asp:Content>
--- BalumaProject Plantilla/BalumaProject Plantilla Frontend/Catalogo.aspx.cs
+++ BalumaProject Plantilla/BalumaProject Plantilla Frontend/Catalogo.aspx.cs
@@ -14,80 +14,116 @@ namespace BalumaProject_Plantilla_Frontend
         public string content;
         private string prod_nombre;
         private float prod_precio;
-        private ProductoCEN productos = new ProductoCEN();
-       
+        private ProductoCEN productoCen = new ProductoCEN();
+        protected IList<ProductoEN> productos;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             IList<ProductoEN> lista_productos = new List<ProductoEN>();
-          
+
             /*
              * Al cargar, la página debe leer los productos que se encuentran disponibles en stock de la
              * base de datos y colocarlos dinámicamente en una tabla
              * */
 
-           lista_productos = productos.DameTodosLosProductos(0, 100);
-           crea_contenido(lista_productos);
+            lista_productos = productoCen.DameTodosLosProductos(0, 100);
+            //crea_contenido(lista_productos);
+            if (!IsPostBack)
+            {
+                productos = productoCen.DameTodosLosProductos(0, 100);
+            }
+            else if (IsPostBack)
+            {
+                productos = productoCen.DameTodosLosProductos(0, 100);
+            }
         }
 
+        protected void BuscarProductos(object sender, EventArgs e)
+        {
+            var opcion = lista.SelectedItem.Text.ToLower();
+            float price;
 
-       
-        
+            if (!opcion.Equals("todos") && precio.Text == String.Empty)
+            {
+                productos = productoCen.DamePorCategoria(opcion);
+            }
+            else if (opcion.Equals("todos") && precio.Text != String.Empty)
+            {
+                price = float.Parse(precio.Text);
+                productos = productoCen.DamePorPrecio(price);
+            }
+            else if (opcion.Equals("todos") && precio.Text == String.Empty)
+            {
+                productos = productoCen.DameTodosLosProductos(0, 100);
+            }
+            else
+            {
+                price = float.Parse(precio.Text);
+                productos = productoCen.DamePorCategoriayPrecio(opcion, price);
+            }
+        }
+
         protected void buscar_productos(object sender, EventArgs e)
         {
-            content = ""; 
+            content = "";
             IList<ProductoEN> lista_busqueda = new List<ProductoEN>();
             bool encontrado = true;
             string opcion = lista.SelectedItem.Text.ToLower();
             float price;
             if (!opcion.Equals("todos") && (precio.Text == null || precio.Text.Equals("")))
             {
-                lista_busqueda = productos.DamePorCategoria(opcion);
+                lista_busqueda = productoCen.DamePorCategoria(opcion);
             }
-            else if (precio.Text!=null||!precio.Text.Equals(""))
+            else if (precio.Text != null || !precio.Text.Equals(""))
             {
                 price = float.Parse(precio.Text);
-                lista_busqueda = productos.DamePorPrecio(price);
+                lista_busqueda = productoCen.DamePorPrecio(price);
             }
-            else 
+            else
             {
                 price = float.Parse(precio.Text);
-                lista_busqueda = productos.DamePorCategoriayPrecio(opcion,price);
+                lista_busqueda = productoCen.DamePorCategoriayPrecio(opcion, price);
             }
 
             if (lista_busqueda == null || lista_busqueda.Count == 0) encontrado = false;
             if (encontrado) { crea_contenido(lista_busqueda); }
             else contenido_no_encontrado();
-           
-           
+
+
+        }
+
+        protected IList<ProductoEN> ObtenerProductos()
+        {
+            return productos;
         }
 
         private void crea_contenido(IList<ProductoEN> lista_contenido)
         {
-           
+
             int cuenta_trío = 0;
-           
+
             content += "<div class=\"grids_of_3\">"; //envoltura por cada trío de productos
-           
+
             foreach (ProductoEN prod in lista_contenido)
             {
                 if (cuenta_trío == 3)
                 {
                     content += "<div class=\"clear\"></div>";
-                   
+
                     content += "<div class=\"grids_of_3 \">";
                     cuenta_trío = 0;
                 }
-              
+
                 content += "<div class=\"grid1_of_3\">";
                 prod_nombre = prod.Nombre;
                 prod_precio = prod.Precio;
                 content += "<a href=\"details.html\"> <img src=\"Content/images/bavaria.jpg\" alt=\"\"/><h3>" + prod_nombre + "</h3> <span class=\"price\">" + prod_precio + "€</span> </a>  ";
-               
+
                 content += "<button OnServerClick = \"anyadeCarrito\">Añadir al carrito</button>";
-               
+
                 content += "</div>";
                 cuenta_trío++;
-               
+
             }
             content += "</div><div class=\"clear\"></div>"; //fin de envoltura
         }
@@ -99,7 +135,7 @@ namespace BalumaProject_Plantilla_Frontend
         protected void anyadeCarrito(object sender, EventArgs e)
         {
             Master.HtmlGenericControl.InnerText = "1";
-       }
+        }
 
     }
 }
